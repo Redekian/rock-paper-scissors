541d17ec51af5520db2f7fc4449dc92d606c8ac8
Redekian <redekiangaming@gmail.com>
2022-07-14 10:58:45 +0300

    Add computerPlay function that generates a random number and uses it to choose rock paper or scissors

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+// Chooses a random number and uses it to select rock paper or scissors
+const computerPlay = () => {
+  const options = ["Rock", "Paper", "Scissors"];
+  const choice = Math.floor(Math.random() * 3);
+  console.log(choice);
+  return options[choice];
+};
+
+console.log(computerPlay());
